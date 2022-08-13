Fixed wheel_calibration to calculate average

Week03-05/operate.py
@@ -3,7 +3,7 @@
 
 # basic python packages
 import numpy as np
-import cv2 
+import cv2
 import os, sys
 import time
 
@@ -30,7 +30,7 @@ class Operate:
         else:
             shutil.rmtree(self.folder)
             os.makedirs(self.folder)
-        
+
         # initialise data parameters
         if args.play_data:
             self.pibot = dh.DatasetPlayer("record")
@@ -47,7 +47,7 @@ class Operate:
         else:
             self.data = None
         self.output = dh.OutputWriter('lab_output')
-        self.command = {'motion':[0, 0], 
+        self.command = {'motion':[0, 0],
                         'inference': False,
                         'output': False,
                         'save_inference': False,
@@ -70,9 +70,9 @@ class Operate:
         self.bg = pygame.image.load('pics/gui_mask.jpg')
 
     # wheel control
-    def control(self):       
+    def control(self):
         if args.play_data:
-            lv, rv = self.pibot.set_velocity()            
+            lv, rv = self.pibot.set_velocity()
         else:
             lv, rv = self.pibot.set_velocity(
                 self.command['motion'])
@@ -88,7 +88,7 @@ class Operate:
         if not self.data is None:
             self.data.write_image(self.img)
 
-    # SLAM with ARUCO markers       
+    # SLAM with ARUCO markers
     def update_slam(self, drive_meas):
         lms, self.aruco_img = self.aruco_det.detect_marker_positions(self.img)
         if self.request_recover_robot:
@@ -126,7 +126,7 @@ class Operate:
         scale = np.loadtxt(fileS, delimiter=',')
         if ip == 'localhost':
             scale /= 2
-        fileB = "{}baseline.txt".format(datadir)  
+        fileB = "{}baseline.txt".format(datadir)
         baseline = np.loadtxt(fileB, delimiter=',')
         robot = Robot(baseline, scale, camera_matrix, dist_coeffs)
         return EKF(robot)
@@ -138,7 +138,7 @@ class Operate:
             self.notification = 'Map is saved'
             self.command['output'] = False
 
-    # paint the GUI            
+    # paint the GUI
     def draw(self, canvas):
         canvas.blit(self.bg, (0, 0))
         text_colour = (220, 220, 220)
@@ -150,7 +150,7 @@ class Operate:
             not_pause = self.ekf_on)
         canvas.blit(ekf_view, (2*h_pad+320, v_pad))
         robot_view = cv2.resize(self.aruco_img, (320, 240))
-        self.draw_pygame_window(canvas, robot_view, 
+        self.draw_pygame_window(canvas, robot_view,
                                 position=(h_pad, v_pad)
                                 )
 
@@ -181,29 +181,29 @@ class Operate:
         view = pygame.surfarray.make_surface(cv2_img)
         view = pygame.transform.flip(view, True, False)
         canvas.blit(view, position)
-    
+
     @staticmethod
     def put_caption(canvas, caption, position, text_colour=(200, 200, 200)):
         caption_surface = TITLE_FONT.render(caption,
                                           False, text_colour)
         canvas.blit(caption_surface, (position[0], position[1]-25))
 
-    # keyboard teleoperation        
+    # keyboard teleoperation
     def update_keyboard(self):
         for event in pygame.event.get():
             ########### replace with your M1 codes ###########
             # drive forward
             if event.type == pygame.KEYDOWN and event.key == pygame.K_UP:
-                pass # TODO: replace with your M1 code to make the robot drive forward
+                self.command['motion'] = [1, 0]
             # drive backward
             elif event.type == pygame.KEYDOWN and event.key == pygame.K_DOWN:
-                pass # TODO: replace with your M1 code to make the robot drive backward
+                self.command['motion'] = [-1, 0]
             # turn left
             elif event.type == pygame.KEYDOWN and event.key == pygame.K_LEFT:
-                pass # TODO: replace with your M1 code to make the robot turn left
+                self.command['motion'] = [0, 1]
             # drive right
             elif event.type == pygame.KEYDOWN and event.key == pygame.K_RIGHT:
-                pass # TODO: replace with your M1 code to make the robot turn right
+                self.command['motion'] = [0, -1]
             ####################################################
             # stop
             elif event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
@@ -251,7 +251,7 @@ class Operate:
             pygame.quit()
             sys.exit()
 
-        
+
 if __name__ == "__main__":
     import argparse
 
@@ -262,11 +262,11 @@ if __name__ == "__main__":
     parser.add_argument("--save_data", action='store_true')
     parser.add_argument("--play_data", action='store_true')
     args, _ = parser.parse_known_args()
-    
-    pygame.font.init() 
+
+    pygame.font.init()
     TITLE_FONT = pygame.font.Font('pics/8-BitMadness.ttf', 35)
     TEXT_FONT = pygame.font.Font('pics/8-BitMadness.ttf', 40)
-    
+
     width, height = 700, 660
     canvas = pygame.display.set_mode((width, height))
     pygame.display.set_caption('ECE4078 2021 Lab')